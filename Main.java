3a7e54b955bc039c67ec39a6741b23ac92998482
jaimeg <jaimegracia2004@gmail.com>
2026-05-07 16:43:58 -0600

    redo the commit. I thought it I change main in java but I didn't

--- Main.java
+++ Main.java
@@ -16,8 +16,9 @@ class Main {
 			System.out.println("5. View inventory sorted by manufacture year.");
 			System.out.println("6. Drone Search");
 			System.out.println("7. ID Search");
-			System.out.println("8. Exit");
-			System.out.println("Please make a selection. You can enter 1-8 to progress... ");
+			System.out.println("8. maintenance");
+			System.out.println("0. Exit");
+			System.out.print("Please make a selection. You can enter 1-8 to progress:");
 			try {
 				menuChoice = Integer.parseInt(scanner.nextLine());
 			}
@@ -56,7 +57,33 @@ class Main {
 				String search = scanner.nextLine();
 				Hangar.searchDroneID(search);
 
-			} else if(menuChoice == 0) {
+			} else if (menuChoice == 8)
+			{
+				System.out.println("welcome maintenance\n type 1 to add drone,\n type 2 to remove,\n or type 3 to display.");
+				String search = scanner.nextLine();
+				while(true)
+				{
+					if(search.equals("1"))
+					{
+						Hangar.addMaintenanceDrone();
+					} else if (search.equals("2"))
+					{
+						Hangar.removeMaintenanceDrone();
+					}
+					else if (search.equals("3"))
+					{
+						Hangar.displayMaintenanceDrone();
+					}
+					System.out.println("are you done(y/n)?");
+					String answer = scanner.nextLine();
+					if(answer.equalsIgnoreCase("y")||answer.equalsIgnoreCase("yes"))
+					{
+						break;
+					}
+				}
+			}
+			if(menuChoice == 0)
+			{
 				System.out.println("Exiting program, safe skies!");
 			}
 		}
